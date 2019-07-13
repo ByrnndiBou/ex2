8d8d6892a359534156c68deea78babf9977b2fea
Byrnndi Bou <byrnndibou@gmail.com>
2019-07-13 14:14:44 +0700

    add tell

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const app = express();
 const port = 3000
 app.get('/', (req, res) => res.send('Byrnndi Bou'))
 app.get('/about', (req, res) => res.send('Index about'))
-app.get('/contact', (req, res) => res.send('Contact'))
+app.get('/test', (req, res) => {
+      res.json({message: "test"})
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
